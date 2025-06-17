6b0dcbc35dd5c71d4d79c07b9efa97addd82350a
Ayushant <theayushant@gmail.com>
2025-06-18 00:59:34 +0530

    Update meta descriptions in _app.js to be more concise

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,14 +14,14 @@ export default function MyApp({ Component, pageProps }) {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://atkind.com" />
         <meta property="og:title" content="atkind - Professional Web Development" />
-        <meta property="og:description" content="Custom web applications and production-ready solutions" />
+        <meta property="og:description" content="Custom software and production-ready solutions" />
         
         {/* Twitter */}
         <meta property="twitter:card" content="summary" />
         <meta property="twitter:url" content="https://atkind.com" />
         <meta property="twitter:title" content="atkind - Professional Web Development" />
-        <meta property="twitter:description" content="Custom web applications and production-ready solutions" />
-        
+        <meta property="twitter:description" content="Custom software and production-ready solutions" />
+
         {/* Canonical link */}
         <link rel="canonical" href="https://atkind.com" />
         
